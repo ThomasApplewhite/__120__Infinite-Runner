71345442994ae0bc7fad3c13318ba194323b5f29
Thomas Applewhite <thomashapplewhite@gmail.com>
2020-04-21 13:47:56 -0700

    Game Set-Up

---
+++ src/main.js
@@ -0,0 +1,14 @@
+/*
+Header Commend Goes Here
+*/
+
+let config = {
+    type: Phaser.CANVAS,
+    width: 640,
+    height: 480,
+    scene: [  ]
+}
+
+let game = new Phaser.Game(config);
+game.settings = {
+}
