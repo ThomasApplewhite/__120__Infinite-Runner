b7c9f5eb39ca9eac8115275fa9d87e29edb624ca
Thomas Applewhite <thomashapplewhite@gmail.com>
2020-04-25 18:19:49 -0700

    Code Reorganization
    Moving specific logic creation to their own methods, to make them easier to find/move later on

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -36,18 +36,6 @@ class Play extends Phaser.Scene{
             'backgroundTile'
             ).setOrigin(0, 0).setScale(5.625);
         
-        //creating invisible walls
-        //don't forget to make these, ya know, actually invisible
-        this.invisibleWallsGroup = this.add.group({
-            classType: Phaser.Physics.Arcade.Sprite,
-            active: true,
-            runChildUpdate: false
-        }).addMultiple([
-            this.physics.add.sprite(-50, config.height/2, 'invisible_wall').setImmovable(),
-            this.physics.add.sprite(config.width+50, config.height/2, 'invisible_wall').setImmovable(),
-            this.physics.add.sprite(config.width/2, (config.height/3)-50, 'invisible_wall_rotated').setImmovable()
-        ]);
-        
         //creating the player
         this.player = new Player(
             this, 
@@ -57,74 +45,14 @@ class Play extends Phaser.Scene{
             0
             ).setOrigin(0, 0);
         
-        //creating the group to hold all the obstacles
-        this.obstacleGroup = this.add.group({
-            classType: Phaser.GameObjects.Sprite.Obstacle,
-            defaultKey: null,               //default texture to use
-            defaultFrame: null,             //default animation frame start to use
-            active: true,
-            maxSize: 15,
-            runChildUpdate: true,
-            createCallback: null,           //what to do when an object is added to the group
-            removeCallback: null,           //what to do when an object is removed from the group
-            createMultipleCallback: null    //what to do when multiple objects are added to the group
-        });
+        //creating object groups
+        this.defineGroups();
 
-        //creating the group to hold all the enemies
-        this.enemyGroup = this.add.group({
-            classType: Phaser.GameObjects.Sprite.Enemy,
-            active: true,
-            maxSize: 5,
-            runChildUpdate: true
-        });
-
-        //creating the group to hold all the attacks
-        this.attackGroup = this.add.group({
-            classType: Phaser.GameObjects.Sprite,
-            active: true,
-            maxSize: -1,
-            runChildUpdate: true
-        })
-
-        //obstacle spawning timer
-        this.obstacleSpawnTimer = this.time.addEvent({
-            delay: 250,                // ms
-            callback: this.createObstacle,
-            //args: [],
-            callbackScope: this,
-            loop: true
-        });
-
-        //enemy spawning timer
-        this.enemySpawnTimer = this.time.addEvent({
-            delay: 500,
-            callback: this.createEnemy,
-            //args: [],
-            callbackScope: this,
-            loop: true
-        });
+        //creating spawning timers
+        this.defineSpawnTimers();
         
         //creating colliders
-        this.physics.add.collider(this.player, this.obstacleGroup, function(player){
-            player.startStun();
-        });
-        this.physics.add.collider(this.player, this.enemyGroup, function(player, enemy){
-            enemy.onAttack(player);
-        });
-            //bUt tHOMAs tHiS Is AN oVeRlAP!!!1!111!! fuck outta here
-            //fuck outta here colliders are ineffective
-        this.physics.add.overlap(this.attackGroup, this.enemyGroup, function(attack, enemy){
-            attack.strike(enemy);
-        })
-        this.physics.add.overlap(this.attackGroup, this.obstacleGroup, function(attack, enemy){
-            //to simulate that it has struck nothing
-            attack.strike(null);
-        })
-        //creating colliders for things that just need to collide
-        this.physics.add.collider(this.player, this.invisibleWallsGroup);
-        this.physics.add.collider(this.enemyGroup, this.obstacleGroup);
-        this.physics.add.collider(this.enemyGroup, this.enemyGroup);
-        //this.physics.add.collider(this.enemyGroup, this.invisibleWallsGroup);
+        this.defineColliders();
         
         //game-over flag
         this.gameOver = false;
@@ -167,5 +95,92 @@ class Play extends Phaser.Scene{
                 )
             );
         }
+    }
+
+
+    //And now, a whole bunch of loading methods. Might just do this in a different file... nah...
+    defineGroups(){
+        //creating invisible walls
+        //don't forget to make these, ya know, actually invisible
+        this.invisibleWallsGroup = this.add.group({
+            classType: Phaser.Physics.Arcade.Sprite,
+            active: true,
+            runChildUpdate: false
+        }).addMultiple([
+            this.physics.add.sprite(-50, config.height/2, 'invisible_wall').setImmovable(),
+            this.physics.add.sprite(config.width+50, config.height/2, 'invisible_wall').setImmovable(),
+            this.physics.add.sprite(config.width/2, (config.height/3)-50, 'invisible_wall_rotated').setImmovable()
+        ]);
+        
+        //creating the group to hold all the obstacles
+        this.obstacleGroup = this.add.group({
+            classType: Phaser.GameObjects.Sprite.Obstacle,
+            defaultKey: null,               //default texture to use
+            defaultFrame: null,             //default animation frame start to use
+            active: true,
+            maxSize: 15,
+            runChildUpdate: true,
+            createCallback: null,           //what to do when an object is added to the group
+            removeCallback: null,           //what to do when an object is removed from the group
+            createMultipleCallback: null    //what to do when multiple objects are added to the group
+        });
+
+        //creating the group to hold all the enemies
+        this.enemyGroup = this.add.group({
+            classType: Phaser.GameObjects.Sprite.Enemy,
+            active: true,
+            maxSize: 5,
+            runChildUpdate: true
+        });
+
+        //creating the group to hold all the attacks
+        this.attackGroup = this.add.group({
+            classType: Phaser.GameObjects.Sprite,
+            active: true,
+            maxSize: -1,
+            runChildUpdate: true
+        });        
+    }
+    defineSpawnTimers(){
+        //obstacle spawning timer
+        this.obstacleSpawnTimer = this.time.addEvent({
+            delay: 250,                // ms
+            callback: this.createObstacle,
+            //args: [],
+            callbackScope: this,
+            loop: true
+        });
+        
+        //enemy spawning timer
+        this.enemySpawnTimer = this.time.addEvent({
+            delay: 500,
+            callback: this.createEnemy,
+            //args: [],
+            callbackScope: this,
+            loop: true
+        });
+    }
+
+    defineColliders(){
+        this.physics.add.collider(this.player, this.obstacleGroup, function(player){
+            player.startStun();
+        });
+        this.physics.add.collider(this.player, this.enemyGroup, function(player, enemy){
+            enemy.onAttack(player);
+        });
+            //bUt tHOMAs tHiS Is AN oVeRlAP!!!1!111!! fuck outta here
+            //fuck outta here colliders are ineffective
+        this.physics.add.overlap(this.attackGroup, this.enemyGroup, function(attack, enemy){
+            attack.strike(enemy);
+        })
+        this.physics.add.overlap(this.attackGroup, this.obstacleGroup, function(attack, enemy){
+            //to simulate that it has struck nothing
+            attack.strike(null);
+        })
+        //creating colliders for things that just need to collide
+        this.physics.add.collider(this.player, this.invisibleWallsGroup);
+        this.physics.add.collider(this.enemyGroup, this.obstacleGroup);
+        this.physics.add.collider(this.enemyGroup, this.enemyGroup);
+        //this.physics.add.collider(this.enemyGroup, this.invisibleWallsGroup);
     }
 }
